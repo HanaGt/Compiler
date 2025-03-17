63f7601f0d418e3c70967a5bc76f54e97881558d
HanaGt <hana.guta-ug@aau.edu.et>
2025-03-17 12:36:13 +0300

    first commit

---
+++ ConsonantVowel- Task 1/test1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    printf("vowel and conso :) ");
+    return 0;
+}
---
+++ CountNum- Task 2/test2.c
@@ -0,0 +1,8 @@
+int x = 42;
+float y = -3.14;
+double z = +5.67;
+int w = -9;
+int main() {
+    printf("vowel and conso :) ");
+    return 3.3;
+}
